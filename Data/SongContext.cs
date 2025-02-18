b29e493378a460478224902997b3f0f3b2878cf0
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-02-18 12:10:56 +0100

    added database connection in program.cs. created a dataContext

---
+++ Data/SongContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using MusicApi.Models;
+
+namespace MusicApi.Data {
+    public class SongContext : DbContext {
+        public SongContext(DbContextOptions<SongContext> options) : base(options) {
+
+        }
+
+        public DbSet<Song> Songs {get; set;}
+        public DbSet<Category> Categories {get; set;}
+    }
+}
