ce695810143db779cc2e9f7971571ed6b9646bde
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2025-02-20 11:49:20 +0100

    justerat kommentarer

--- Data/SongContext.cs
+++ Data/SongContext.cs
@@ -1,8 +1,12 @@
 using Microsoft.EntityFrameworkCore;
-using MusicApi.Models;
+using MusicApi.Models;//import models
 
 namespace MusicApi.Data {
+
+    //SongContext inherit from DbContext. Connection between application and database
     public class SongContext : DbContext {
+
+        //constructor handles DbContext options and send to base class DbContext
         public SongContext(DbContextOptions<SongContext> options) : base(options) {
 
         }
--- Models/Song.cs
+++ Models/Song.cs
@@ -4,7 +4,7 @@ namespace MusicApi.Models {
     public class Song {
 
         //Properties
-        public int Id { get; set; } //PK
+        public int Id { get; set; } 
 
         [Required]
         public string? Artist { get; set; }
@@ -13,11 +13,9 @@ namespace MusicApi.Models {
         public string? Title { get; set; }
 
         [Required]
-        public int Length { get; set; }
+        public int Length { get; set; } //length in seconds
 
         [Required]
-        public string? Category { get; set; }
-     
-        
+        public string? Category { get; set; }       
     }
 }
